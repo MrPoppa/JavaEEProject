93c4d65fdb47233a340c579abe73169c7cf3c36b
Benjamin Bengtsson <benjaminbenjibengtsson@gmail.com>
2015-09-20 09:52:54 +0200

    Modified peconfigured sql queries used to create the database. AttendanceList not yet implemented.

--- JavaEEProject-ejb/src/conf/db_create.sql
+++ JavaEEProject-ejb/src/conf/db_create.sql
@@ -1,100 +1,93 @@
-CREATE TABLE student(
-id INTEGER PRIMARY KEY,
-firstName VARCHAR(20),
-lastName VARCHAR(20),
-gender VARCHAR(15),
-age INTEGER)
-
-CREATE TABLE teacher(
-id INTEGER PRIMARY KEY,
-firstName VARCHAR(20),
-lastName VARCHAR(20),
-gender VARCHAR(15),
-age INTEGER,
-userName VARCHAR(20),
-password VARCHAR(30),
-salary INTEGER,
-dateHired DATE)
-
-CREATE TABLE principal(
-id INTEGER PRIMARY KEY,
-firstName VARCHAR(20),
-lastName VARCHAR(20),
-gender VARCHAR(15),
-age INTEGER,
-userName VARCHAR(20),
-password VARCHAR(30),
-salary INTEGER,
-dateHired DATE)
-
-
-CREATE TABLE admin(
-id INTEGER PRIMARY KEY,
-firstName VARCHAR(20),
-lastName VARCHAR(20),
-gender VARCHAR(15),
-age INTEGER,
-userName VARCHAR(20),
-password VARCHAR(30),
-salary INTEGER,
-dateHired DATE)
-
-CREATE TABLE course(
-id INTEGER PRIMARY KEY,
-name VARCHAR(30),
-code VARCHAR(10),
-level VARCHAR(10),
-language VARCHAR(15),
-period VARCHAR(15),
-maxNumberOfStudents INTEGER
+CREATE TABLE ContactInformation(
+id INTEGER NOT NULL AUTO_INCREMENT,
+streetName VARCHAR(32),
+zipCode VARCHAR(16),
+city VARCHAR(64),
+phoneNumber VARCHAR(16),
+emailAddress VARCHAR(64),
+PRIMARY KEY (id)
 )
 
-CREATE TABLE ContactInformation(
-id INTEGER PRIMARY KEY,
-streetName VARCHAR(30),
-zipCode VARCHAR(10),
-city VARCHAR(50),
-phoneNumber VARCHAR(20),
-emailAddress VARCHAR(40)
+CREATE TABLE Student(
+id INTEGER NOT NULL AUTO_INCREMENT,
+firstName VARCHAR(32),
+lastName VARCHAR(32),
+gender VARCHAR(16),
+age INTEGER,
+contact_id INTEGER,
+PRIMARY KEY (id),
+CONSTRAINT fk_student_contact FOREIGN KEY (contact_id) REFERENCES Contactinformation(id)
+)
+
+CREATE TABLE Course(
+id INTEGER NOT NULL AUTO_INCREMENT,
+courseName VARCHAR(32),
+courseCode VARCHAR(16),
+courseLevel VARCHAR(16),
+courseLanguage VARCHAR(16),
+coursePeriod VARCHAR(16),
+maxNumberOfStudents INTEGER,
+PRIMARY KEY (id)
+)
+
+CREATE TABLE Course_Student (
+student_id INTEGER,
+course_id INTEGER,
+CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES Student(id),
+CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES Course(id)
+)
+
+CREATE TABLE Teacher(
+id INTEGER NOT NULL AUTO_INCREMENT,
+firstName VARCHAR(32),
+lastName VARCHAR(32),
+gender VARCHAR(16),
+age INTEGER,
+userName VARCHAR(16),
+password VARCHAR(16),
+salary INTEGER,
+dateHired DATE,
+contact_id INTEGER,
+PRIMARY KEY (id),
+CONSTRAINT fk_teacher_contact FOREIGN KEY (contact_id) REFERENCES Contactinformation(id)
+)
+
+CREATE TABLE Principal(
+id INTEGER NOT NULL AUTO_INCREMENT,
+firstName VARCHAR(32),
+lastName VARCHAR(32),
+gender VARCHAR(16),
+age INTEGER,
+userName VARCHAR(16),
+password VARCHAR(16),
+salary INTEGER,
+dateHired DATE,
+contact_id INTEGER,
+PRIMARY KEY (id),
+CONSTRAINT fk_principal_contact FOREIGN KEY (contact_id) REFERENCES Contactinformation (id)
+)
+
+CREATE TABLE Administrator (
+id INTEGER NOT NULL AUTO_INCREMENT,
+firstName VARCHAR(20),
+lastName VARCHAR(20),
+gender VARCHAR(15),
+age INTEGER,
+userName VARCHAR(20),
+password VARCHAR(30),
+salary INTEGER,
+dateHired DATE,
+PRIMARY KEY (id)
+)
+
+CREATE TABLE AttendanceLists(
+id INTEGER NOT NULL AUTO_INCREMENT,
+attendanceDate DATE,
+PRIMARY KEY (id)
 )
 
 CREATE TABLE AttendanceList(
-id INTEGER PRIMARY KEY,
-date DATE
-)
+student_id INTEGER,
+attendanceList_id INTEGER,
 
-CREATE TABLE Student_Course(
-student_id INTEGER REFERENCES Student(id),
-course_id INTEGER REFERENCES Course(id),
-PRIMARY KEY (student_id, course_id)
-)
-
-ALTER TABLE Student
-ADD CONSTRAINT fk_contact_information FOREIGN KEY(id) REFERENCES Course(id)
-
-ALTER TABLE Teacher
-ADD COLUMN teacher_course_id INTEGER
-
-ALTER TABLE Teacher
-ADD CONSTRAINT fk_teacher_course_id FOREIGN KEY(teacher_course_id) REFERENCES Course(id)
-
-ALTER TABLE Course
-ADD CONSTRAINT fk_attendanceList_id FOREIGN KEY(id) REFERENCES AttendanceList(id)
-
-ALTER TABLE AttendanceList
-ADD COLUMN attendancelist_student_id INTEGER
-
-ALTER TABLE AttendanceList
-ADD CONSTRAINT fk_attendancelist_student_id FOREIGN KEY(attendancelist_student_id) REFERENCES Student(id)
-
-ALTER TABLE Student_Course
-ADD CONSTRAINT fk_student_id FOREIGN KEY(Student_id) REFERENCES Student(id)
-
-ALTER TABLE Student_Course
-ADD CONSTRAINT fk_course_id FOREIGN KEY(Course_id) REFERENCES Course(id)
-
-CREATE TABLE Student_AttendanceList(
-student_id INTEGER REFERENCES Student(id),
-attendanceList_id INTEGER REFERENCES AttendanceList(id),
-PRIMARY KEY (student_id, attendanceList_id)
 )
