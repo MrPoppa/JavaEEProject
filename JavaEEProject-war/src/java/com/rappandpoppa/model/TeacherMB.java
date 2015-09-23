d2d5eb6e8c500a837160b3f6bd06b4ba0d3d70cb
Andersrapp <anders.rapp@gmail.com>
2015-09-23 10:57:04 +0200

    removed faulty import.

--- JavaEEProject-war/src/java/com/rappandpoppa/model/TeacherMB.java
+++ JavaEEProject-war/src/java/com/rappandpoppa/model/TeacherMB.java
@@ -1,9 +1,13 @@
 package com.rappandpoppa.model;
 
+import com.rappandpoppa.entities.Course;
+import com.rappandpoppa.entities.Student;
 import com.rappandpoppa.model.origin.Employee;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
 
 /**
  *
@@ -12,13 +16,94 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class TeacherMB extends Employee {
 
-    private List<CourseMB> courses = new ArrayList<>();
+    private List<Course> courses = new ArrayList<>();
+    private String chosenCourseName;
+    private Date chosenDate;
+    private Date periodStartDate;
+    private Date periodEndDate;
+    private List<Date> courseDates;
+    private List<Student> attendingStudentsByCourseDate;
+    @Inject
+    CourseMB courseMB;
+    @Inject
+    AttendanceListMB attendanceListMB;
 
-    public List<CourseMB> getCourses() {
+    public List<Course> getCourses() {
         return courses;
     }
 
-    public void setCourses(List<CourseMB> courses) {
+    public void setCourses(List<Course> courses) {
         this.courses = courses;
+    }
+
+    public String getChosenCourseName() {
+        return chosenCourseName;
+    }
+
+    public void setChosenCourseName(String chosenCourseName) {
+        this.chosenCourseName = chosenCourseName;
+    }
+
+    public Date getChosenDate() {
+        return chosenDate;
+    }
+
+    public void setChosenDate(Date chosenDate) {
+        this.chosenDate = chosenDate;
+    }
+
+    public List<Date> getCourseDates() {
+        return courseDates;
+    }
+
+    public void setCourseDates(List<Date> courseDates) {
+        this.courseDates = courseDates;
+    }
+
+    public List<Student> getAttendingStudentsByCourseDate() {
+        return attendingStudentsByCourseDate;
+    }
+
+    public void setAttendingStudentsByCourseDate(List<Student> attendingStudentsByCourseDate) {
+        this.attendingStudentsByCourseDate = attendingStudentsByCourseDate;
+    }
+
+    public Date getPeriodStartDate() {
+        return periodStartDate;
+    }
+
+    public void setPeriodStartDate(Date periodStartDate) {
+        this.periodStartDate = periodStartDate;
+    }
+
+    public Date getPeriodEndDate() {
+        return periodEndDate;
+    }
+
+    public void setPeriodEndDate(Date periodEndDate) {
+        this.periodEndDate = periodEndDate;
+    }
+
+    public void viewAllCourses() {
+        courses.clear();
+        courses = teacherFacade.findTeacherCourses();
+    }
+
+    public void onCourseChange() {
+        if (chosenCourseName != null && !chosenCourseName.equals("")) {
+//            Integer courseId = courseFacade.findIdByCourseName(chosenCourseName);
+            dates = attendanceListFacade.findAllDatesByCourse(courseMB.getId());
+        } else {
+            dates = new ArrayList<>();
+        }
+    }
+
+    public void getStudentsByCourseDate() {
+        attendingStudentsByCourseDate.clear();
+        attendingStudentsByCourseDate = attendanceListMB.attendanceListFacade.findAllStudentsByCourseDate(chosenDate);
+    }
+
+    public void viewAttendingStudentsByCourseDate() {
+
     }
 }
